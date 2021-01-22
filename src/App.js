da850c04009b10e48afb1b78fc1da8b949f32e06
Ganesh Koilada <itsmeganeshcse@gmail.com>
2021-01-22 13:50:24 +0530

    feat: bit initial template
    Template for Bit components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ProductList from './components/product-list';
-import TopBar from './components/top-bar';
+// import ProductList from './components/product-list';
+// import TopBar from './components/top-bar';
 import './App.css';
+
+import Input from './share/atoms/input'
 
 function App() {
   return (
     <>
-      <TopBar className="top-bar" />
+      {/* <TopBar className="top-bar" />
       <div className="container">
         <ProductList />
-      </div>
+      </div> */}
+      <Input/>
     </>
   );
 }
---
+++ src/share/components/atoms/input/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {StyledInput} from './styles'
+const Input = () => {
+    return (
+        <StyledInput>
+            
+        </StyledInput>
+    )
+}
+
+Input.defaultProps={
+
+}
+
+
+export default Input
